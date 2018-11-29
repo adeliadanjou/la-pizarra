80099c5bcb61450c82b42f6a8edbf8b504376328
Jaime <herrerotajaime@gmail.com>
2018-11-29 12:05:10 +0100

    cambios

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const flash      = require("connect-flash");
     
 
 mongoose
-  .connect('mongodb://localhost/la-pizarra', {useNewUrlParser: true})
+  .connect(process.env.URL, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
