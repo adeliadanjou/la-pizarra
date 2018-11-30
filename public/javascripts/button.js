ac98f253aad178e24d31b5c89e8d32e64eaa93a1
cgandiaf <c.gandia.f@gmail.com>
2018-11-30 09:18:27 +0100

    Viernes

--- public/javascripts/button.js
+++ public/javascripts/button.js
@@ -1,6 +1,8 @@
 $(document).ready(() => {
 
 
+//no dejar funciones anónimas
+
   document.querySelector('.next').onclick = function () {
     // let container = document.getElementById('first');
     // //container.classList.remove("on");
@@ -17,13 +19,13 @@ $(document).ready(() => {
     document.getElementById('first').classList.remove("off");
   }
 
-  document.querySelector('.addMeeting').onclick = function () {
-    console.log('Pasa por el adddd')
-    // let container = document.getElementById('first');
-    // //container.classList.remove("on");
-    // container.classList.add("off");
-    document.getElementById('form-meeting').classList.remove("off");
-  }
+  // document.querySelector('.addMeeting').onclick = function () {
+  //   console.log('Pasa por el adddd')
+  //   // let container = document.getElementById('first');
+  //   // //container.classList.remove("on");
+  //   // container.classList.add("off");
+  //   document.getElementById('form-meeting').classList.remove("off");
+  // }
 
   // document.querySelector('.addMeeting').onclick = function () {
   //   // let container = document.getElementById('first');
